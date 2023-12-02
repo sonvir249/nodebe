d3e955940a4de3f381b641b1a0a85447973eab62
sonvir249 <39142-sonvir249@users.noreply.drupalcode.org>
2023-12-02 18:54:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('twitter page')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Login page</h1>')
+})
+
+app.get('/chai', (req, res) => res.send('<h1>Chai page</h1>'))
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
